3be1590c0dc363fba702c68288b619b7577cc713
sergiourzedojr <sergiourzedojr@gmail.com>
2024-04-22 14:29:37 -0300

    first commit

---
+++ api_geo.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import time
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
+
+
+# Download and prepare the dataset
+url = 'https://dadosabertos.aneel.gov.br/dataset/f9336ee6-7562-4c92-880a-d7217df6cc94/resource/28686698-544b-48e2-af58-ad4f7616246c/download/projetos-eficiencia-energetica-empresa.csv'
+df = pd.read_csv(url, encoding='ISO-8859-1', sep=';', on_bad_lines='skip')
+
+# Data cleaning and type conversion
+df['DatGeracaoConjuntoDados'] = pd.to_datetime(df['DatGeracaoConjuntoDados'])
+df['DatInicioProjeto'] = pd.to_datetime(df['DatInicioProjeto'])
+df['DatConclusaoProjeto'] = pd.to_datetime(df['DatConclusaoProjeto'])
+df.replace({',': '.'}, regex=True, inplace=True)  # Replace commas in numeric fields
+numeric_fields = ['VlrRetiradaDemandaPontaTotal', 'VlrRcbGlobal', 'VlrEnergiaEconomizadaTotal', 'VlrCustoTotal']
+df[numeric_fields] = df[numeric_fields].astype(float)
+df = df[df['VlrCustoTotal'] != 0]  # Filter out rows with zero cost
+
+# Calculating a score and filtering data
+epsilon = 0.01
+df['Score'] = (df['VlrEnergiaEconomizadaTotal'] + df['VlrRetiradaDemandaPontaTotal']) / (df['VlrCustoTotal'] + epsilon)
+df = df.dropna(subset=['Score'])
+df = df.sort_values('Score', ascending=False)
+threshold_score = df['Score'].quantile(0.95)
+df = df[df['Score'] > threshold_score]
+df['Score'] = 1 + (df['Score'] - df['Score'].min()) * (10 - 1) / (df['Score'].max() - df['Score'].min())
+
+# Geocoding locations using Nominatim
+geolocator = Nominatim(user_agent="unique_user_agent", timeout=10)
+def get_location(name):
+    try:
+        location = geolocator.geocode(name)
+        return location.address if location else None, location.latitude if location else None, location.longitude if location else None
+    except (GeocoderTimedOut, GeocoderUnavailable):
+        time.sleep(5)
+        return get_location(name)  # Retry
+
+# Apply geocoding to unique agent names
+unique_agents = df['NomAgente'].unique()
+location_data = {name: get_location(name) for name in unique_agents}
+df['Address'], df['Latitude'], df['Longitude'] = zip(*df['NomAgente'].map(location_data))
+
+# Save updated DataFrame to a new CSV file
+df.to_csv('df_unidos.csv', index=False)
+
+# Analyze project durations
+date_info = pd.read_csv('df_unidos.csv', usecols=['DatInicioProjeto', 'DatConclusaoProjeto'], parse_dates=['DatInicioProjeto', 'DatConclusaoProjeto'])
+date_info['Duration_Years'] = (date_info['DatConclusaoProjeto'] - date_info['DatInicioProjeto']).dt.days / 365.25
+duration_years_mean = date_info['Duration_Years'].mean()
+
+# Save summary data to CSV files
+pd.DataFrame([date_info['DatInicioProjeto'].min()], columns=['DatInicioProjeto']).to_csv('projeto1.csv', index=False)
+pd.DataFrame([date_info['DatConclusaoProjeto'].max()], columns=['DatConclusaoProjeto']).to_csv('termino_ultimo_proj.csv', index=False)
+pd.DataFrame([duration_years_mean], columns=['Duration_Years']).to_csv('Duration_Years_projects_mean.csv', index=False)
---
+++ pages/3_Tabelas.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import time
+import webbrowser  # Import needed to use webbrowser for opening URLs
+
+
+# Set Streamlit page config
+st.set_page_config(
+    layout="wide",
+    page_title="Eficiência Energética Labdados"
+)
+
+# App main interface
+st.markdown("# tabelas! ⚡")
+
+# Ensure session state initialization
+if 'Duration_Years' not in st.session_state:
+    st.session_state['Duration_Years'] = load_duration_data()
+
+if 'df_proj' not in st.session_state:
+    st.session_state['df_proj'] = load_proj1_data()
+
+if 'df_uproj' not in st.session_state:
+    st.session_state['df_uproj'] = load_uproj_data()
+
+if 'df_unidos' not in st.session_state:
+    st.session_state['df_unidos'] = load_unidos_data()
+
+
+st.markdown("---")  # Markdown syntax for a horizontal line
+
+
+
+# Display data in the app
+#st.session_state['df_unidos'] = st.session_state['df_unidos'].drop(columns=['Unnamed: 0'])
+st.session_state['df_unidos'] = st.session_state['df_unidos']
+st.write("tabela de dados - tratados", st.session_state['df_unidos'])
+
+
+
+
+
